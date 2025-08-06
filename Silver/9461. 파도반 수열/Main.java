bafbbce0f187eb8eeb2b0cfd1056af04ba1926ac
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-06 15:33:18 +0900

    Create Main.java

---
+++ Silver/9461. 파도반 수열/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		long[] dp = new long[101];
+
+		dp[1] = dp[2] = dp[3] = 1;
+		for (int i = 4; i < 101; i++) {
+			dp[i] = dp[i - 2] + dp[i - 3];
+		}
+
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			sb.append(dp[n]).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
