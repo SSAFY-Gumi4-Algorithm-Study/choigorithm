25037d9b15243f560e0b6a71402a8e5f157e1f60
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-08 14:57:01 +0900

    [실버3] 11659. 구간 합 구하기 4

--- Silver/11659. 구간 합 구하기 4/Main.java
+++ Silver/11659. 구간 합 구하기 4/Main.java
@@ -7,26 +7,25 @@ class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n];
-		int[] dp = new int[n];
-		
+		int[] arr = new int[n + 1];
+		int[] dp = new int[n + 1];
+
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		dp[0] = arr[0];
-		for (int i = 1; i < n; i++) {
+
+		for (int i = 1; i <= n; i++) {
 			dp[i] = dp[i - 1] + arr[i];
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			
-			sb.append(dp[end - 1] - dp[start - 1] + arr[start - 1]).append("\n");
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+
+			sb.append(dp[j] - dp[i - 1]).append("\n");
 		}
 		System.out.println(sb);
 	}
