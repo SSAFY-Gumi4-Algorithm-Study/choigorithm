ba67d89c9b09e4ba41426c6ac2c44e395a9f4ac4
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-14 10:20:47 +0900

    [실버 5] 2740. 행렬 곱셈

---
+++ Silver/2740. 행렬 곱셈/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, ans = 0;
+	static int[] s, w;
+
+	public static void main(String[] args) throws IOException {
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] a = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		st = new StringTokenizer(br.readLine());
+		m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] b = new int[m][k];
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < k; j++) {
+				b[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int[][] ans = new int[n][k];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k; j++) {
+				for (int l = 0; l < m; l++) {
+					ans[i][j] += a[i][l] * b[l][j];
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int[] rows : ans) {
+			for (int i : rows) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
