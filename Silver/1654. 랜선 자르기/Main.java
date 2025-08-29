090cdde6b3c52d161c524b4ec441b540f257e1ca
Youngeun Choi <youngeun.choi26@gmail.com>
2025-08-29 14:38:19 +0900

    [실버 2] 1654. 랜선 자르기

---
+++ Silver/1654. 랜선 자르기/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[k];
+		long left = 0;
+		long right = 0;
+
+		for (int i = 0; i < k; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			right = Math.max(right, arr[i]) + 1;
+		}
+
+		while (left < right) {
+			long cnt = 0;
+			long mid = (left + right) / 2;
+
+			for (int i : arr) {
+				cnt += i / mid;
+			}
+
+			if (cnt < n) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		System.out.println(left - 1);
+	}
+}
