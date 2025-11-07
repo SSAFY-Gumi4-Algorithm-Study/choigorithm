636459b71ae3b171021d55fc8d7e81b61905f08d
Youngeun Choi <youngeun.choi26@gmail.com>
2025-11-07 10:34:50 +0900

    [브론즈] 5337. 웰컴

---
+++ Bronze/5337. 웰컴/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb =  new StringBuilder();
+		sb.append(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+		
+		System.out.print(sb);
+	}
+}
