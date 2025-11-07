f53cfe8cbbfe879927e7ee0e2ca0163f55b207ac
Youngeun Choi <youngeun.choi26@gmail.com>
2025-11-07 10:44:08 +0900

    [브론즈] 2576. 홀수

---
+++ Bronze/2576. 홀수/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0, min = Integer.MAX_VALUE;
+		for (int i = 0; i < 7; i++) {
+			int n = Integer.parseInt(br.readLine());
+
+			if (n % 2 == 0)
+				continue;
+
+			sum += n;
+			min = Math.min(min, n);
+		}
+
+		if (min == Integer.MAX_VALUE)
+			System.out.println(-1);
+		else
+			System.out.println(sum + "\n" + min);
+	}
+}
