7abe6464f3adf9ebdbc25fe7b5b00ed48d850cef
Youngeun Choi <youngeun.choi26@gmail.com>
2025-12-08 10:37:13 +0900

    [골드] 5427. 불

---
+++ Gold/5427. 불/Main.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Point {
+        int r, c, time;
+
+        public Point(int r, int c, int time) {
+            this.r = r;
+            this.c = c;
+            this.time = time;
+        }
+    }
+
+    static int h, w;
+    static Queue<Point> fQ;
+    static Queue<Point> jQ;
+    static boolean[][] visited;
+    static int[][] fireMap;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            fireMap = new int[h][w];    // 불 확산 시간 (시작 시간: 1초)
+            visited = new boolean[h][w];
+            fQ = new ArrayDeque<>();
+            jQ = new ArrayDeque<>();
+
+            for (int i = 0; i < h; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    char c = s.charAt(j);
+
+                    if (c == '#') {
+                        fireMap[i][j] = -1;
+                    } else if (c == '*') {
+                        fQ.add(new Point(i, j, 1));
+                        fireMap[i][j] = 1;
+                    } else if (c == '@') {
+                        jQ.add(new Point(i, j, 1));
+                        visited[i][j] = true;
+                    }
+                }
+            }   // 입력 끝
+
+            fMove();
+
+            int ans = jMove();
+            System.out.println(ans == -1 ? "IMPOSSIBLE" : ans);
+        }
+    }
+
+    static void fMove() {
+        while (!fQ.isEmpty()) {
+            Point cur = fQ.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
+
+                if (nr < 0 || nc < 0 || nr >= h || nc >= w || fireMap[nr][nc] != 0)
+                    continue;
+
+                fireMap[nr][nc] = cur.time;
+                fQ.add(new Point(nr, nc, cur.time + 1));
+            }
+        }
+    }
+
+    static int jMove() {
+        while (!jQ.isEmpty()) {
+            Point cur = jQ.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
+
+                if (nr < 0 || nc < 0 || nr >= h || nc >= w)
+                    return cur.time;
+
+                if (visited[nr][nc] || fireMap[nr][nc] == -1
+                        || (fireMap[nr][nc] != 0 && fireMap[nr][nc] <= cur.time))    // 불 확산 <= 현재 시간
+                    continue;
+
+                jQ.add(new Point(nr, nc, cur.time + 1));
+                visited[nr][nc] = true;
+            }
+        }
+
+        return -1;
+    }
+}
